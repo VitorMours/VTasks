ffda1e9a36c569a0deab3b921e9683d212cb1fc6
Joao Vitor <jvrezendemoura@gmail.com>
2025-06-06 15:59:06 -0300

    Creating the nav sidebar aminmations

--- src/views/home.py
+++ src/views/home.py
@@ -1,11 +1,19 @@
 from flask import Blueprint, redirect, render_template, flash 
 from flask.views import MethodView, View
-
 bp = Blueprint("home", __name__)
 
-class DashboardView(View):
+class HomeView(View):
 	def dispatch_request(self) -> str:
-		return render_template("dashboard.html")
+		return render_template("home.html", active_page="home")
 
+class DashboardView(MethodView):
+	def get(self) -> str:
+		return render_template("dashboard.html", active_page="dashboard")
 
-bp.add_url_rule("/home/", view_func=DashboardView.as_view("dashboard"))
+class TodoView(MethodView):
+	def get(self) -> str:
+		return render_template("todo.html", active_page="todo")
+
+bp.add_url_rule("/home/", view_func=HomeView.as_view("home"))
+bp.add_url_rule("/dashboard/", view_func=DashboardView.as_view("dashboard"))
+bp.add_url_rule("/todo/", view_func=TodoView.as_view("todo"))
