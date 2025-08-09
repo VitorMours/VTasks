1cbc2863f9d26e8814c2a1a13525b596fe67b219
Joao Vitor <jvrezendemoura@gmail.com>
2025-08-09 13:11:18 -0300

    feature(note): Creating the form responsable to be the on that give the ability to create the user note.

--- src/forms/__init__.py
+++ src/forms/__init__.py
@@ -6,3 +6,8 @@ class BaseAuthForm(FlaskForm):
   email = EmailField("Email", validators=[DataRequired(message="Please type your email")])
   password = PasswordField("Password", validators=[DataRequired(message="Please type your password")])
   submit = SubmitField("Submit", validators=[DataRequired()])
+
+class NoteForm(FlaskForm):
+  title = StringField("Title", validators=[DataRequired(message="Please, do not create a titless note...")])
+  content = StringField("Content", render_kw={"placeholder":"Type the content of your note..."})
+  create = SubmitField("Create Note", validators=[DataRequired()])
--- src/models/user_model.py
+++ src/models/user_model.py
@@ -1,26 +1,30 @@
-from sqlalchemy import String
+from sqlalchemy import String, UUID
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from typing import List
 import uuid
 from . import db
 
 class User(db.Model):
     __tablename__ = "user"
 
-    id = db.Column(String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
-    first_name = db.Column(String(50), nullable=False)
-    last_name = db.Column(String(50))
-    email = db.Column(String(100), nullable=False, unique=True)
-    password = db.Column(String(256), nullable=False)
-    tasks = db.relationship("task", back_populates="user")
-
+    id: Mapped[str] = mapped_column(UUID(as_uuid=False), primary_key=True, default=lambda: str(uuid.uuid4()))
+    first_name: Mapped[str] = mapped_column(String(50), nullable=False)
+    last_name: Mapped[str | None] = mapped_column(String(50))
+    email: Mapped[str] = mapped_column(String(100), nullable=False, unique=True)
+    password: Mapped[str] = mapped_column(String(256), nullable=False)
+    tasks: Mapped[List["Task"]] = relationship(back_populates="user") # type:ignore
+    notes: Mapped[List["Note"]] = relationship(back_populates="owner") # type:ignore
+    
     @property
     def full_name(self) -> str:
-        if self.first_name != "":
-            full_name = f"{self.first_name} {self.last_name}"
-            return full_name.strip()
-        return "O usuario ainda nao possui nome cadastrado"
+        if self.first_name and self.last_name:
+            return f"{self.first_name} {self.last_name}".strip()
+        if self.first_name:
+            return self.first_name
+        return "O usuário ainda não possui nome cadastrado"
 
     def __repr__(self) -> str:
-        pass
+        return f"<User(id='{self.id}', email='{self.email}')>"
 
     def __str__(self) -> str:
         return f"{self.first_name} {self.last_name}: {self.email}"
