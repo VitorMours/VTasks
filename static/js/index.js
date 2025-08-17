54333690f9a212e52ddbc29c0226bccdfcc292bb
Vitor Moura <jvrezendemoura@gmail.com>
2025-08-17 20:58:57 -0300

    Creating the notes grid

--- static/js/index.js
+++ static/js/index.js
@@ -1,20 +1,18 @@
-// Making all the imports
-import task from "../js/task.js";
-import TaskManager from "../js/taskManager.js";
-// Getting all the references
+const { handleImageError } = require('./notes.js');
+const task = require('../js/task.js');
+const TaskManager = require('../js/taskManager.js');
+
+
 const allList = document.getElementById("todo-all-list");
 const doneList = document.getElementById("todo-done-list");
 const activeList = document.getElementById("todo-active-list");
+
 const deleteIconPath = document.body.dataset.deleteIcon;
 
+document.addEventListener('DOMContentLoaded', () => {
+    const images = document.querySelectorAll('.card-img-top');
 
-import addConclusionInteraction from "./task.js";
-
-const allTasks = document.getElementById("todo-all-tab-content");
-const doneTasks = document.getElementById("todo-done-tab-content");
-const activeTasks = document.getElementById("todo-active-tab-content");
-
-
-addConclusionInteraction(allTasks);
-addConclusionInteraction(doneTasks);
-addConclusionInteraction(activeTasks);
+    images.forEach(img => {
+        img.onerror = img.classList.add("image-failed");
+    });
+});
---
+++ test/integration/home_page_integration_test.py
@@ -0,0 +1,43 @@
+import pytest 
+from bs4 import BeautifulSoup
+from enum import Enum
+from playwright.sync_api import sync_playwright
+
+class RequestMethod(Enum):
+    get = 'GET'
+    post = 'POST'       
+    put = 'PUT'
+    delete = 'DELETE'
+
+def verify_request_method(route, request, method: RequestMethod):
+    assert request.method == method.value
+    route.continue_()
+
+def test_home_page_get(client):
+    response = client.get('/')
+    assert response.headers['Content-Type'] == 'text/html; charset=utf-8'
+    assert response.status_code == 200 
+
+def test_home_page_post(client):
+    with sync_playwright() as p:
+        browser = p.chromium.launch()
+        page = browser.new_page()
+
+        page.route("**/",  lambda route, request: verify_request_method(route, request, RequestMethod.post))
+
+        response = client.post('http://localhost:5000/')
+        assert response.status_code == 405
+        soup = BeautifulSoup(response.data, 'html.parser')
+        error_title = soup.find(id="error-message")
+        redirect_link = soup.find(name="a")
+        assert error_title is not None
+        assert error_title.text == "Houve algum problema, por favor volte para a tela de login..."
+        assert redirect_link.text == "Home Page"
+
+def test_home_page_put(client):
+    response = client.put('/')
+    assert response.status_code == 405 
+
+def test_home_page_delete(client):
+    response = client.delete('/')
+    assert response.status_code == 405 
