1dd5cd2913d3b0c4c3c8e2a994982be5070a9079
Vitor Moura <jvrezendemoura@gmail.com>
2025-08-17 20:58:57 -0300

    Creating the notes grid

--- static/js/index.js
+++ static/js/index.js
@@ -1,9 +1,18 @@
-// Making all the imports
-import task from "../js/task.js";
-import TaskManager from "../js/taskManager.js";
-// Getting all the references
+const { handleImageError } = require('./notes.js');
+const task = require('../js/task.js');
+const TaskManager = require('../js/taskManager.js');
+
+
 const allList = document.getElementById("todo-all-list");
 const doneList = document.getElementById("todo-done-list");
 const activeList = document.getElementById("todo-active-list");
+
 const deleteIconPath = document.body.dataset.deleteIcon;
 
+document.addEventListener('DOMContentLoaded', () => {
+    const images = document.querySelectorAll('.card-img-top');
+
+    images.forEach(img => {
+        img.onerror = img.classList.add("image-failed");
+    });
+});
