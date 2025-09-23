198ccc791d22a399229688a59d42b04084feeb2b
Vitor Moura <jvrezendemoura@gmail.com>
2025-09-23 16:14:34 -0300

    tests: Creating creational tasks related

--- src/repositories/task_repository.py
+++ src/repositories/task_repository.py
@@ -6,12 +6,25 @@ from src.models.user_model import User
 class TaskRepository:
     
     @staticmethod 
-    def create(task_data: dict, user: User) -> None:
+    def create(task_data: dict, user_id: User) -> None:
         """
         Create a new task in the database based on the user as the foreign key.
         """
+        needed_fields = ["task","task_description","task_conclusion"]
         
-    @staticmethod
+        for field in needed_fields:
+            if field not in task_data.keys():
+                return False
+
+        new_task = Task(
+                task = task_data["task"],
+                task_description = task_data["task_description"],
+                task_conclusion = task_data["task_conclusion"],
+                user_id = user_id
+                )
+        return new_task
+    
+    @staticmethod   
     def update() -> None:
         pass
     @staticmethod
@@ -29,4 +42,9 @@ class TaskRepository:
     @staticmethod 
     def get_by_id() -> None:
         pass
-        
+
+    def __repr__(self) -> str:
+        return "<TaskRepository>"
+
+    def __str__(self) -> str:
+        return "<TaskRepository>"
--- tests/unit/repositories/test_task_repository.py
+++ tests/unit/repositories/test_task_repository.py
@@ -1,8 +1,9 @@
 from src.repositories.task_repository import TaskRepository
 from src.repositories.user_repository import UserRepository
 from src.models.task_model import Task 
-
+from src.models.user_model import User
 import importlib
+
 class TestTaskRepository:
     
     def test_if_its_running(self) -> None:
@@ -12,22 +13,39 @@ class TestTaskRepository:
         module = importlib.import_module("src.repositories.task_repository")
         assert module is not None
         assert hasattr(module, "TaskRepository")
-        
+
     def test_if_task_repository_is_callable(self) -> None:
         module = importlib.import_module("src.repositories.task_repository")
         assert module is not None
         assert callable(module.TaskRepository)
-        
+
     def test_if_task_repository_have_the_standard_methods(self) -> None: 
         standard_methods = ["create","update","delete","get_all","get_by_email", "get_by_id"]
         repository = TaskRepository()
         for method in standard_methods:
             assert hasattr(repository, method)        
-            
+
+    def test_if_repository_has_str_and_repr_representation(self, app) -> None: 
+        with app.app_context():
+            repository = TaskRepository()
+            assert str(repository) == "<TaskRepository>"
+            assert repr(repository) == "<TaskRepository>"
+
     def test_creating_task(self, app, create_random_user_dict, create_random_task_dict) -> None:
         with app.app_context():
             new_user = UserRepository.create(create_random_user_dict)
+            assert isinstance(new_user, User)
             created_task = TaskRepository.create(create_random_task_dict, new_user.id)
-            assert isinstance(created_task,  Task)
-        
-        
+            assert created_task
+
+    def test_if_creating_task_return_task(self, app, create_random_user_dict, create_random_task_dict) -> None:
+        with app.app_context():
+            new_user = UserRepository.create(create_random_user_dict)
+            assert isinstance(new_user, User)
+            created_task = TaskRepository.create(create_random_task_dict, new_user.id)
+            assert isinstance(created_task, Task)
+
+
+    # TODO: Restart from this point
+    def test_if_can_create_and_search_for_task(self, app, create_random_user_dict, create_random_task_dict) -> None:
+        assert True
